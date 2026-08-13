f6e95b01194e8bea440000ff5cf4be35ed96742b
callmeloony <37262544+callmeloony@users.noreply.github.com>
2026-08-14 00:04:02 +0300

    Update __init__.py

--- custom_components/ykd_led/__init__.py
+++ custom_components/ykd_led/__init__.py
@@ -1,12 +1,26 @@
-"""The YKD-RF LED integration."""
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS: list[Platform] = [Platform.LIGHT]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up YKD-RF LED from a config entry."""
-    await hass.config_entries.async_forward_entry_setups(entry, ["light"])
+    """Налаштування інтеграції з UI ConfigEntry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, ["light"])
+    """Видалення інтеграції."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id, None)
+
+    return unload_ok
